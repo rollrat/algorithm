fd746b38618551c4aff6b4ae8ffe64ed8e78633e
violet-dev <violet.dev.master@gmail.com>
2021-11-25 09:15:01 +0000

    tree: implements simple tree

--- src/tree.hpp
+++ src/tree.hpp
@@ -2,16 +2,35 @@
 
 #pragma once
 
-template <typename T>
-struct linked_binary_tree_node {
-    typedef class linked_binary_tree_node<T> this_type;
-    this_type* left;
-    this_type* right;
-    this_type* parent;
-    T value;
+template <typename T> struct linked_binary_tree_node {
+  typedef class linked_binary_tree_node<T> this_type;
+  this_type *left;
+  this_type *right;
+  this_type *parent;
+  T value;
+
+  linked_binary_tree_node(T value) : value(value) {}
 };
 
-template <typename T>
-class linked_binary_tree {
+template <typename T> class linked_binary_tree {
+  typedef class linked_binary_tree_node<T> node_type;
+  node_type *root_node;
 
+public:
+  node_type *root() const { return root_node; }
+
+  void refresh(const T &value) {
+    remove_node(this->root_node);
+    root_node = make_node(value);
+  }
+
+  node_type *make_node(const T &type) {
+    node_type *node = new node_type(type);
+    return node;
+  }
+
+  void remove_node(node_type *node) {
+    if (node == nullptr)
+      return;
+  }
 };
--- tests/tree_test.cpp
+++ tests/tree_test.cpp
@@ -1,20 +1,23 @@
 //
 
 #include "../src/tree.hpp"
+#include <iostream>
 
 int main() {
   linked_binary_tree<int> tree;
 
   // init tree
-  tree.refresh();
+  tree.refresh(0);
 
   // get root and push some nodes
   auto cur_node = tree.root();
-  cur_node.left = tree.make_node(4);
-  cur_node.left.right(tree.make_node(5));
+  cur_node->left = tree.make_node(4);
+  cur_node->left->right = tree.make_node(5);
 
-  if (tree.root().left.right != 5)
+  if (tree.root()->left->right->value != 5)
     return -1;
+
+  std::cout << "success";
 
   return 0;
 }
