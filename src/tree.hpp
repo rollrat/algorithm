a7a5ecb5b8525c370b1599cc5f9423dccfa00f4a
violet-dev <violet.dev.master@gmail.com>
2021-11-25 09:28:52 +0000

    tree: add some tree header

--- src/tree.hpp
+++ src/tree.hpp
@@ -34,3 +34,50 @@ public:
       return;
   }
 };
+
+template <typename T> class binary_tree {
+  T *array;
+  int capacity = 1;
+  int depth = 0;
+
+  /*
+       O     <== depth 0
+
+    Capacity = 1
+
+       O     <== depth 0
+     /  \    <== depth 1
+    O   O    <== depth 2
+
+    Capacity = 3
+
+          O
+        /  \
+       O    O
+     /  \ /  \
+    O   OO   O   <== depth 3
+
+    Capacity = 7
+
+    Capacity = 2**depth - 1
+  */
+
+public:
+  void ensure_depth(int depth) { ensure_capacity(depth); }
+
+  T &operator[](int index) { return this->array[index]; }
+
+private:
+  void ensure_capacity(int depth) {
+    if (this->depth < depth) {
+      this->depth += 1;
+      int tsize = 1 << this->depth - 1;
+      T *tarr = new T[tsize];
+      if (this->array != nullptr)
+        for (int i = 0; i < this->capacity; i++)
+          tarr[i] = array[i];
+      this->capacity = tsize;
+      this->array = tarr;
+    }
+  }
+};
